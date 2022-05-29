6149ba802625da53b864527b2008c7d07bb653d3
Lemon <516330797@qq.com>
2022-05-30 00:03:31 +0800

    feat: 使用 express.static

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('yyy'))
 
 app.use((request, response, next) => {
   console.log('request.body')
