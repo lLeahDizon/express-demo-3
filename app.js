3129000453d4982da9cef0a53dd9cb668803a94d
Lemon <516330797@qq.com>
2022-05-29 23:58:53 +0800

    feat: 使用 express.json

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.use((request, response, next) => {
+  console.log('request.body')
+  console.log(request.body)
+  console.log(typeof request.body)
+  response.send('hi')
+  next()
+})
+
+app.listen(3000, () => {
+  console.log('listen 成功')
+})
