e56b11f2f4275dc6098330fc020e1a62e7838d47
Lemon <516330797@qq.com>
2022-06-08 23:35:06 +0800

    feat: 使用 app.use 部分 api

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
 
+app.set('case sensitive routing', true)
+app.set('views', 'lemon')
+app.set('view engine', 'ejs')
+
 app.use(express.json())
-app.use(express.static('yyy'))
+// app.use(express.static('yyy'))
 app.use(express.urlencoded())
+
+app.get('/test', (req, res, next) => {
+  res.render('test', {pageTitle: 'lemon'})
+})
+
+app.get('/style.css', (req, res, next) => {
+  res.send('style.css')
+})
+
+app.get('/STYLE.css', (req, res, next) => {
+  res.send('STYLE.css')
+})
 
 app.use((request, response, next) => {
   console.log('request.body')
