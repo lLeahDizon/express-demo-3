b2aedd93e99c1086e65d87fccf9681daab541ff4
杨海涛 <516330797@qq.com>
2023-03-07 21:00:30 +0800

    feat: req.xxx API

--- app.js
+++ app.js
@@ -2,29 +2,12 @@ const express = require('express')
 const app = express()
 const fn1 = require('./fn1')
 
-app.set('case sensitive routing', true)
 app.set('views', 'lemon')
 app.set('view engine', 'ejs')
-app.locals.title = '我的个人网站'
 
-app.use(express.json())
-// app.use(express.static('yyy'))
-app.use(express.urlencoded())
-
-app.use(fn1)
-
-app.get('/style.css', (req, res, next) => {
-  res.send('style.css')
-})
-
-app.get('/STYLE.css', (req, res, next) => {
-  res.send('STYLE.css')
-})
-
-app.use((request, response, next) => {
-  console.log('request.body')
-  console.log(request.body)
-  console.log(typeof request.body)
+app.get('/users/:id', (request, response, next) => {
+  console.log('request.xhr')
+  console.log(request.xhr)
   response.send('hi')
   next()
 })
